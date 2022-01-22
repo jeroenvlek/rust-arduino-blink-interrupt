327545d1850d11f09bb8fe602ad0ca5eb761183d
Jeroen Vlek <jeroen@perceptivebits.com>
2022-01-22 14:19:54 +0100

    move impl from different project

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    /*
+     * For examples (and inspiration), head to
+     *
+     *     https://github.com/Rahix/avr-hal/tree/next/examples
+     *
+     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
+     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
+     * examples available.
+     */
+
+    let mut led = pins.d3.into_output();
+
+    loop {
+        (0..20).map(|i| i * 100).for_each(|ms| {
+            led.toggle();
+            arduino_hal::delay_ms(ms as u16);
+        });
+        (20..0).map(|i| i * 100).for_each(|ms| {
+            led.toggle();
+            arduino_hal::delay_ms(ms as u16);
+        });
+    }
+}
