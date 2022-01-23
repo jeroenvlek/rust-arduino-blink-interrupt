ecadfd9cc8cf8fb1f10860cb48014778a8b047ff
Jeroen Vlek <jeroen@perceptivebits.com>
2022-01-23 13:53:22 +0100

    switch to array impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::port::{mode, Pin};
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -8,31 +9,25 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    let mut led0 = pins.d2.into_output();
-    let mut led1 = pins.d3.into_output();
-    let mut led2 = pins.d4.into_output();
-    let mut led3 = pins.d5.into_output();
+    let mut leds: [Pin<mode::Output>; 4] = [
+        pins.d2.into_output().downgrade(),
+        pins.d3.into_output().downgrade(),
+        pins.d4.into_output().downgrade(),
+        pins.d5.into_output().downgrade(),
+    ];
 
     loop {
-        (0..20).map(|i| i * 100).for_each(|ms| {
-            led0.toggle();
-            arduino_hal::delay_ms(ms as u16);
-            led1.toggle();
-            arduino_hal::delay_ms(ms as u16);
-            led2.toggle();
-            arduino_hal::delay_ms(ms as u16);
-            led3.toggle();
-            arduino_hal::delay_ms(ms as u16);
+        (0..10).map(|i| i * 100).for_each(|ms| {
+            leds.iter_mut().for_each(|led| {
+                led.toggle();
+                arduino_hal::delay_ms(ms as u16);
+            })
         });
-        (20..0).map(|i| i * 100).for_each(|ms| {
-            led0.toggle();
-            arduino_hal::delay_ms(ms as u16);
-            led1.toggle();
-            arduino_hal::delay_ms(ms as u16);
-            led2.toggle();
-            arduino_hal::delay_ms(ms as u16);
-            led3.toggle();
-            arduino_hal::delay_ms(ms as u16);
+        (10..0).map(|i| i * 100).for_each(|ms| {
+            leds.iter_mut().for_each(|led| {
+                led.toggle();
+                arduino_hal::delay_ms(ms as u16);
+            })
         });
     }
 }
