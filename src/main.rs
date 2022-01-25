a1100cca4ad9a4a52e942c43ec6adc4fa22d77da
Jeroen Vlek <jeroen@perceptivebits.com>
2022-01-25 22:27:30 +0100

    add interrupts, but not working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 #![no_std]
 #![no_main]
+#![feature(abi_avr_interrupt)]
 
-use arduino_hal::port::{mode, Pin};
 use panic_halt as _;
 use core::ops::Range;
+use core::sync::atomic::{AtomicBool, Ordering};
+use arduino_hal::port::{mode, Pin};
+use either::*;
 
+static REVERSE: AtomicBool = AtomicBool::new(false);
+
+fn is_reversed() -> bool {
+    return avr_device::interrupt::free(|_| { REVERSE.load(Ordering::SeqCst) });
+}
+
+#[avr_device::interrupt(atmega328p)]
+fn INT0() {
+    avr_device::interrupt::free(|_| {
+        let current = REVERSE.load(Ordering::SeqCst);
+        REVERSE.store(!current, Ordering::SeqCst);
+    });
+}
 
 fn blink_for_range(range : Range<u16>, leds : &mut[Pin<mode::Output>]) {
     range.map(|i| i * 100).for_each(|ms| {
-        leds.iter_mut().for_each(|led| {
+        let iter = if is_reversed() {
+            Left(leds.iter_mut().rev())
+        } else {
+            Right(leds.iter_mut())
+        };
+        iter.for_each(|led| {
             led.toggle();
             arduino_hal::delay_ms(ms as u16);
         })
@@ -20,12 +41,16 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
+    pins.d2.into_pull_up_input(); // is this necessary?
+
     let mut leds: [Pin<mode::Output>; 4] = [
-        pins.d2.into_output().downgrade(),
         pins.d3.into_output().downgrade(),
         pins.d4.into_output().downgrade(),
         pins.d5.into_output().downgrade(),
+        pins.d6.into_output().downgrade(),
     ];
+
+    unsafe { avr_device::interrupt::enable() };
 
     loop {
         blink_for_range(0..10, &mut leds);
